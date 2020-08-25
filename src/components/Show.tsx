1873cc8aa9536e702d14f9579d89898110925384
ntn <neethan.bala@gmail.com>
2020-08-25 18:33:11 -0400

    Retrieve image for each episode

--- src/components/Show.tsx
+++ src/components/Show.tsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from "react";
+import tmdbService from "../services/tmdb";
 
 const Show = (props: any) => {
+  const [image, setImage] = useState("../images/NoImgFound.jpg");
+
+  useEffect(() => {
+    // getImg();
+  }, []);
+
+  const getImg = async () => {
+    const res = await tmdbService.getPosterForShow(props.show.ids.tmdb);
+    setImage(res);
+  };
+  console.log(image);
+
   return (
     <div>
       <h1>{props.episode.title}</h1>
+      <img src={image} width="200" height="300"></img>
     </div>
   );
 };
