47174581db990499bbc0365b1208bf51ba289667
ntn <neethan.bala@gmail.com>
2020-08-25 17:32:06 -0400

    Add service to retrieve posters for shows and movies

---
+++ src/services/tmdb.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+const baseUrl = "https://api.themoviedb.org/3";
+const clientId = "605bfb7955646c5982b3a027ebea657e";
+
+const constructImageUrl = async (fileUrl) => {
+  const baseImageUrl = "http://image.tmdb.org/t/p/";
+  const imageSize = "original";
+
+  return `${baseImageUrl}${imageSize}${fileUrl}`;
+};
+
+const getPosterForShow = async (showId) => {
+  const response = await axios.get(
+    `${baseUrl}/tv/${showId}/images?api_key=${clientId}&language=en`
+  );
+
+  let imageUrl = "../images/NoImgFound.jpg";
+  const images = response.data.posters;
+  if (images) {
+    imageUrl = constructImageUrl(images[0].file_path);
+  }
+  return imageUrl;
+};
+
+const getPosterForMovies = async (movieId) => {
+  const response = await axios.get(
+    `${baseUrl}/movie/${movieId}/images?api_key=${clientId}&language=en`
+  );
+
+  let imageUrl = "../images/NoImgFound.jpg";
+  const images = response.data.posters;
+  if (images) {
+    imageUrl = constructImageUrl(images[0].file_path);
+  }
+  return imageUrl;
+};
+
+export default {
+  getPosterForShow,
+  getPosterForMovies,
+};
