093465cd2c8569da98b513d6b477cdf99a0e0576
ntn <neethan.bala@gmail.com>
2020-08-26 16:40:47 -0400

    Add react router and sample routes for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -8,7 +9,11 @@ import traktService from "./services/trakt";
 
 const App = () => {
   const [token, setToken] = useState("");
+  const { SubMenu } = Menu;
   const { Header, Content, Footer } = Layout;
+  const padding = {
+    padding: 5,
+  };
 
   useEffect(() => {
     const accessToken = getAccessToken();
@@ -20,10 +25,32 @@ const App = () => {
 
   return (
     <Layout className="layout">
-      <Header>Test</Header>
+      <Header className="header">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <Link style={padding} to="/">
+              home
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key="2">
+            <Link style={padding} to="/history">
+              history
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key="3">
+            <Link style={padding} to="/recommendations">
+              recommendations
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
       <Content style={{ padding: "0 50px" }}>
         <div style={{ height: "calc(100vh - 55px)" }}>
-          {token ? <Home /> : <Login />}
+          <Switch>
+            <Route path="/">{token ? <Home /> : <Login />}</Route>
+          </Switch>
         </div>
       </Content>
       <Footer style={{ textAlign: "center" }}>Created by Neethan</Footer>
