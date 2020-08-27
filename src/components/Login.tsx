77ae3e5c74b9a5249228a0bcd42b79d8006f36f9
ntn <neethan.bala@gmail.com>
2020-08-27 16:29:21 -0400

    Fixed login url being used for auth

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,9 +4,9 @@ import logo from "../images/trakt.png";
 
 const Login = () => {
   const loginUrl =
-    process.env.env === "prod"
-      ? "http://fierce-woodland-56626.herokuapp.com/login"
-      : "http://localhost:8080/login";
+    process.env.ENV === "test"
+      ? "http://localhost:8080/api/login"
+      : "http://fierce-woodland-56626.herokuapp.com/api/login";
 
   return (
     <div>
